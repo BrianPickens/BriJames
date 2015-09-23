edcd1a765aa6688f6d39e589b5dda22b52964b8b
Brian Pickens <brianpickens53@gmail.com>
2015-09-23 15:14:35 -0700

    controller

---
+++ BriJamesTeamProject/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/MoveCharacter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCharacter : MonoBehaviour {
+
+	public float rotationSpeed = 250f;
+
+	private Transform _myTransform;
+
+
+	void Awake(){
+		_myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0) {
+			_myTransform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+		}
+
+	}
+}
