a8ecd01d69ae07d0ae8b126f1f86a30d126744b5
Brian Pickens <brianpickens53@gmail.com>
2015-09-24 14:28:34 -0700

    side controller

---
+++ BriJamesTeamProject/Assets/Scripts/LockedCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockedCamera : MonoBehaviour {
+
+	public Transform target;
+	public float height;
+	public float walkDistance;
+	public float smoothTime = 0.3f;
+
+	private Transform _myTransform;
+	private Vector3 velocity = Vector3.zero;
+
+	void Awake(){
+		_myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		CameraStart ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPosition = target.TransformPoint (new Vector3 (0, 5, -10));
+		_myTransform.position = Vector3.SmoothDamp (_myTransform.position, targetPosition, ref velocity, smoothTime);
+	
+	}
+
+	public void CameraStart(){
+		_myTransform.position = new Vector3 (target.position.x, target.position.y + height, target.position.z - walkDistance);
+		_myTransform.LookAt (target);
+	}
+}
---
+++ BriJamesTeamProject/Assets/Scripts/MoveSide.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSide : MonoBehaviour {
+
+	public float gravity = 20f;
+	public float moveSpeed = 5f;
+	public float rotationSpeed = 250f;
+	public float strafeSpeed = 4f;
+	public float jumpHeight = 8f;
+	public CollisionFlags _collisionFlags;
+	
+	private Vector3 _moveDirection;
+	private Transform _myTransform;
+	private CharacterController _controller;
+	
+	void Awake(){
+		_myTransform = transform;
+		_controller = GetComponent<CharacterController> ();
+	}
+	// Use this for initialization
+	void Start () {
+		_moveDirection = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+//		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0) {
+//			_myTransform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+//		}
+		
+		if (_controller.isGrounded) {
+			
+			_moveDirection = new Vector3(Input.GetAxis("Strafe"),0, Input.GetAxis("Vertical"));
+			_moveDirection = _myTransform.TransformDirection(_moveDirection).normalized;
+			_moveDirection *= moveSpeed;
+			
+			if(Input.GetKeyDown(KeyCode.Space)){
+				_moveDirection.y += jumpHeight;
+			}
+		}	
+		
+		else {
+			if((_collisionFlags & CollisionFlags.CollidedBelow) == 0){
+				
+			}
+		}
+		_moveDirection.y -= gravity * Time.deltaTime;
+		_collisionFlags = _controller.Move(_moveDirection * Time.deltaTime);
+		
+	}
+}
