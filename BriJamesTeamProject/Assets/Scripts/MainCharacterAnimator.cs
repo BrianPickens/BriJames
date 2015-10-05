7b0762c70a1e730ed333910511678b5c0fa3aaae
Brian Pickens <brianpickens53@gmail.com>
2015-10-04 23:55:43 -0700

    Character animation script

---
+++ BriJamesTeamProject/Assets/Scripts/MainCharacterAnimator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterAnimator : MonoBehaviour {
+
+	Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.A)) {
+			anim.SetBool ("Left", true);
+		}
+
+		if (Input.GetKeyUp (KeyCode.A)) {
+			anim.SetBool ("Left", false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			anim.SetBool ("Right", true);
+		}
+		if (Input.GetKeyUp (KeyCode.D)) {
+			anim.SetBool ("Right", false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			anim.SetBool ("Back", true);
+		}
+		if (Input.GetKeyUp (KeyCode.W)) {
+			anim.SetBool ("Back", false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			anim.SetBool ("Front", true);
+		}
+		if (Input.GetKeyUp (KeyCode.S)) {
+			anim.SetBool ("Front", false);
+		}
+
+
+	}
+}
