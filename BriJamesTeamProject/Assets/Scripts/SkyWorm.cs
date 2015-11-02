acf7305eab88bc39400c048bb1f0285f032ed23c
Brian Pickens <brianpickens53@gmail.com>
2015-11-01 23:26:29 -0800

    skyworms

--- BriJamesTeamProject/Assets/Scripts/SkyWorm.cs
+++ BriJamesTeamProject/Assets/Scripts/SkyWorm.cs
@@ -3,23 +3,26 @@ using System.Collections;
 
 public class SkyWorm : MonoBehaviour {
 
-	public Transform _myTransform;
+	public Vector3 _startTransform;
 	public Vector3 _myTarget;
 	private bool hide;
 
 	// Use this for initialization
 	void Start () {
-		_myTransform = transform;
-		_myTarget = new Vector3 (_myTransform.position.x, _myTransform.position.y - 5f, _myTransform.position.z);
+		hide = false;
+		_startTransform = transform.position;
+		_myTarget = new Vector3 (transform.position.x, transform.position.y - 5f, transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		if (hide) {
-			transform.position = Vector3.Lerp(transform.position, _myTarget, 1 * Time.deltaTime);
-		} else {
-			transform.position = Vector3.Lerp(_myTarget, _myTransform.position, 1 * Time.deltaTime);
+			transform.position = Vector3.Lerp(transform.position, _myTarget, 1f * Time.deltaTime);
+		} 
+
+		if (!hide) {
+			transform.position = Vector3.Lerp(transform.position, _startTransform, 1f * Time.deltaTime);
 		}
 
 	}
@@ -27,12 +30,14 @@ public class SkyWorm : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Player") {
 			hide = true;
+
 		}
 	}
 
 	void OnTriggerExit(Collider other){
-		if (other.gameObject.tag == "PLayer") {
+		if (other.gameObject.tag == "Player") {
 			hide = false;
+
 		}
 	}
 }
