08cda41144fda78c64b335a33d77dcc55ab7a030
Brian Pickens <brianpickens53@gmail.com>
2015-11-02 02:12:32 -0800

    basic Dialogue in

---
+++ BriJamesTeamProject/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+	
+	public GameObject DialogueBox;
+	public GameObject CharacterSprite;
+	public Text CharacterText;
+	public Sprite NPC1Sprite;
+	
+
+	// Use this for initialization
+	void Start () {
+		DialogueBox.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Npc1(){
+		DialogueBox.SetActive (true);
+		CharacterText.text = "I Dare YOU to Jump!";
+		CharacterSprite.GetComponent<Image> ().sprite = NPC1Sprite;
+	}
+
+	public void EndDialogue(){
+		DialogueBox.SetActive (false);
+	}
+}
