a117e28eccb1b6eca772ebbff0e7cb55641ef53d
Brian Pickens <brianpickens53@gmail.com>
2015-11-08 18:56:58 -0800

    Animation offsets

---
+++ BriJamesTeamProject/Assets/Scripts/CowAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CowAnimation : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		anim.SetFloat("CowOffset", Random.Range (0f,1f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
