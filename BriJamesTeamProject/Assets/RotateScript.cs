a9823d41e7fd39e8b03ab9a146323809f650adfd
justdeleteit <just404it@gmail.com>
2015-11-13 02:13:26 -0800

    background camera & spin

---
+++ BriJamesTeamProject/Assets/RotateScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+    public float speedR;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0 , speedR * Time.deltaTime);
+    }
+}
